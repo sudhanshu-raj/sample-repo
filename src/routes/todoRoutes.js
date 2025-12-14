a8b0374630c3bfc87d82e428318043e7ee801166
sudhanshu_raj <rajsudhanshu9431@gmail.com>
2025-12-14 12:03:56 +0530

    Test

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -10,6 +10,7 @@ router.delete('/:id', todoController.deleteTodo);
 
 router.get("/hello", (req,res) => {
     res.send("Hey there 🙋 !")
+    console.log("Test line")
 })
 
 router.get("/health",(req,res) =>{
